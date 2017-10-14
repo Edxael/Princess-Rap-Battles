ed46dff0a51bff29bc2d01d48fb7e0fc62bdd4d1
Edxael <zerphyr.r@gmail.com>
2017-10-14 08:11:28 -0600

    update ReadMe.md

--- src/comps/00-MainComp.jsx
+++ src/comps/00-MainComp.jsx
@@ -12,7 +12,7 @@ class MainComp extends React.Component {
         <img style={avaSty} src={Avalon} alt="Miss Pic"/>
         <br/><br/>
         <p>Whitney Avalon is an American actress, writer, producer, singer, and rapper best known for creating the Princess Rap Battle series on YouTube, where her channel has over 307 million views. She uploaded her first YouTube video in September 2010</p>
-        <p>To see more of her vides here is a link to her YouTube channel:<a href="https://www.youtube.com/channel/UCNyJk0eoESO3CxAmrktAB5w" target="_blank" ><strong>Whitney Avalon</strong></a> </p>
+        <p>To see more of her videos follow this link to her YouTube channel:<a href="https://www.youtube.com/channel/UCNyJk0eoESO3CxAmrktAB5w" target="_blank" ><strong>Whitney Avalon</strong></a> </p>
         <p>With no more delays, enjoy the RAPS....</p>
     </div>
     )
